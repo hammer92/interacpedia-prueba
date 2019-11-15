758f15c80b9aaddd2f3f73b8e349a52096c6e38c
hammer <jjpb92@gmail.com>
2019-11-14 23:11:43 -0500

    produccion actualizacion login

--- resources/js/pages/auth/login.vue
+++ resources/js/pages/auth/login.vue
@@ -1,67 +1,69 @@
 <template>
-  <div class="row">
-    <div class="col-lg-8 m-auto">
-      <card :title="$t('login')">
-        <form @submit.prevent="login" @keydown="form.onKeydown($event)">
-          <!-- Email -->
-          <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
-            <div class="col-md-7">
-              <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
-              <has-error :form="form" field="email" />
+    <div class="hold-transition login-page w-100">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">{{$t('login')}}</p>
+
+                <form  @submit.prevent="login" @keydown="form.onKeydown($event)">
+                    <div class="input-group mb-3">
+                        <input v-model="form.email" type="email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" :placeholder="$t('email')">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                        <has-error :form="form" field="email" />
+                    </div>
+                    <div class="input-group mb-3">
+                        <input  v-model="form.password" type="password" :class="{ 'is-invalid': form.errors.has('password') }" class="form-control" :placeholder="$t('password')">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                        <has-error :form="form" field="password" />
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <checkbox v-model="remember" name="remember">
+                                    {{ $t('remember_me') }}
+                                </checkbox>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-12 text-right">
+                            <v-button :loading="form.busy">
+                                {{ $t('login') }}
+                            </v-button>
+<!--                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>-->
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <p class="mb-1">
+                    <router-link :to="{ name: 'password.request' }" class="small ml-auto my-auto">
+                        {{ $t('forgot_password') }}
+                    </router-link>
+                </p>
             </div>
-          </div>
-
-          <!-- Password -->
-          <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('password') }}</label>
-            <div class="col-md-7">
-              <input v-model="form.password" :class="{ 'is-invalid': form.errors.has('password') }" class="form-control" type="password" name="password">
-              <has-error :form="form" field="password" />
-            </div>
-          </div>
-
-          <!-- Remember Me -->
-          <div class="form-group row">
-            <div class="col-md-3" />
-            <div class="col-md-7 d-flex">
-              <checkbox v-model="remember" name="remember">
-                {{ $t('remember_me') }}
-              </checkbox>
-
-              <router-link :to="{ name: 'password.request' }" class="small ml-auto my-auto">
-                {{ $t('forgot_password') }}
-              </router-link>
-            </div>
-          </div>
-
-          <div class="form-group row">
-            <div class="col-md-7 offset-md-3 d-flex">
-              <!-- Submit Button -->
-              <v-button :loading="form.busy">
-                {{ $t('login') }}
-              </v-button>
-
-              <!-- GitHub Login Button -->
-              <login-with-github />
-            </div>
-          </div>
-        </form>
-      </card>
+            <!-- /.login-card-body -->
+        </div>
     </div>
-  </div>
+    <!-- /.login-box -->
+
+    </div>
 </template>
 
 <script>
 import Form from 'vform'
-import LoginWithGithub from '~/components/LoginWithGithub'
 
 export default {
   middleware: 'guest',
-
-  components: {
-    LoginWithGithub
-  },
+    layout:'basic',
 
   metaInfo () {
     return { title: this.$t('login') }
